1456b8f2b89e3c8abd1a8826d03a6e5652c5c17b
Konstantin T <tconsta@tconsta.com>
2021-04-28 22:36:13 +0700

    allowed hosts * debug

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@r5!aghi%mynvdo&sw)uhb17r(b*p_a&66fv*x(y%(&-3eyffd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
