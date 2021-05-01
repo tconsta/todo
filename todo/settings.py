e697caef20c17b592789cdfe378497251b29e236
Konstantin T <tconsta@tconsta.com>
2021-05-01 15:57:16 +0700

    'HOST': 'db'

--- todo/settings.py
+++ todo/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'dev_only',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
